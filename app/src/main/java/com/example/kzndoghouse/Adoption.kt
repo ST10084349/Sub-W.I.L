26865b90616c9f81fcf954196551bd9a45254b08
ST10084349 <ST10084349@vcconnect.edu.za>
2023-10-30 09:28:37 +0200

    INITIAL Commit

---
+++ app/src/main/java/com/example/kzndoghouse/Adoption.kt
@@ -0,0 +1,11 @@
+package com.example.kzndoghouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Adoption : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adoption)
+    }
+}
